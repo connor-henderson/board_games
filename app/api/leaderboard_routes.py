666d3168e17cb23ece350c84de2fb340afe7e86d
Connor <connorchenderson9@gmail.com>
2021-06-28 22:46:31 -0400

    general file cleanup

--- app/api/leaderboard_routes.py
+++ app/api/leaderboard_routes.py
@@ -8,12 +8,12 @@ leaderboard_routes = Blueprint('leaderboards', __name__)
 
 @leaderboard_routes.route('/')
 def all_games():
-    sudoku_scores = User.query.order_by(User.sudoku_score).with_entities(User.username, User.sudoku_score).limit(10).all()
+    sudoku_scores = User.query.order_by(User.sudoku_score.desc()).with_entities(User.username, User.sudoku_score).limit(10).all()
     return { "sudoku_scores": sudoku_scores }
 
 @leaderboard_routes.route('/<game_name>')
 def one_game(game_name):
-    # game_name = "sudoku_scores"
-    # scores = User.query.order_by(User[game_name]).with_entities(User.username, User[game_name]).limit(10).all()
-    # return { game_name: scores }
-    return ""
+    if game_name == "sudoku":
+        scores = User.query.order_by(User.sudoku_score.desc()).with_entities(User.username, User.sudoku_score).all()
+
+    return { "scores": scores }
