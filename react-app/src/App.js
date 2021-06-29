2d8fd91dd0a1bc93164ff93b7c3fc6e32866b2a0
Connor <connorchenderson9@gmail.com>
2021-06-29 16:32:45 -0400

    restyled navbar and dropdown

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import LoginForm from "./components/auth/LoginForm";
-import SignUpForm from "./components/auth/SignUpForm";
+import LoginForm from "./components/Auth/LoginForm";
+import SignUpForm from "./components/Auth/SignUpForm";
 import NavBar from "./components/Navigation/";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import { authenticate } from "./store/session";
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
+      <NavBar loaded={loaded}/>
       <Switch>
         <Route path="/login" exact={true}>
           <LoginForm />
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,19 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import LogoutButton from "../auth/LogoutButton";
+import ProfileButton from "./ProfileButton";
 import logo from "../../assets/images/logo.png";
 import "./Navigation.css";
+import { useSelector } from "react-redux";
 
-const NavBar = () => {
+const NavBar = ({ loaded }) => {
+  const user = useSelector((state) => state.session.user);
+
+  let sessionLinks;
+  if (user) {
+    sessionLinks = <ProfileButton user={user} />;
+  } else {
+    sessionLinks = (
+      <>
+        <div>
+          <NavLink
+            to="/login"
+            exact={true}
+            className="navbar-top__auth-login"
+            activeClassName="active"
+          >
+            Sign in
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/sign-up"
+            exact={true}
+            className="navbar-top__auth-sign-up"
+            activeClassName="active"
+          >
+            Get Started
+          </NavLink>
+        </div>
+      </>
+    );
+  }
+
   return (
     <nav>
       <div className="navbar navbar-top">
         <div className="navbar-top__logo">
-          <NavLink
-            to="/"
-            exact={true}
-            activeClassName="active"
-          >
+          <NavLink to="/" exact={true} activeClassName="active">
             <img
               src={logo}
               alt="logo containing go, chess, and sudoku pieces"
@@ -21,31 +50,7 @@ const NavBar = () => {
             />
           </NavLink>
         </div>
-        <div className="navbar-top__buttons">
-          <div>
-            <NavLink
-              to="/login"
-              exact={true}
-              className="navbar-top__auth-login"
-              activeClassName="active"
-            >
-              Sign in
-            </NavLink>
-          </div>
-          <div>
-            <NavLink
-              to="/sign-up"
-              exact={true}
-              className="navbar-top__auth-sign-up"
-              activeClassName="active"
-            >
-              Get Started
-            </NavLink>
-          </div>
-          <div>
-            <LogoutButton />
-          </div>
-        </div>
+        <div className="navbar-top__buttons">{loaded && sessionLinks}</div>
       </div>
       <div className="navbar navbar-bottom">
         <NavLink to="/" exact={true} activeClassName="active">
