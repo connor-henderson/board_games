5a905e2207213c087cf7d02d925fa8a35f48b47d
Connor <connorchenderson9@gmail.com>
2021-07-02 20:42:35 -0400

    added Sudoku board features and debugging

--- react-app/src/components/Sudoku/Square.js
+++ react-app/src/components/Sudoku/Square.js
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from "react";
 
-export default function Square({ i, j, num, solution, board }) {
+export default function Square({
+  i,
+  j,
+  num,
+  solution,
+  board,
+  clicked,
+  setClicked,
+}) {
   const [guess, setGuess] = useState(num);
   const [gameWon, setGameWon] = useState(false);
 
   useEffect(() => {
-    setGuess(num)
+    setGuess(num);
+    setGameWon(false);
   }, [num]);
 
   useEffect(() => {
     board[i][j] = guess;
-
     if (JSON.stringify(board) === JSON.stringify(solution)) setGameWon(true);
   }, [guess]);
+
+  useEffect(() => {
+    const winMessage = document.querySelector(".win-message");
+    if (gameWon) {
+      winMessage.classList.remove("--hidden");
+    } else {
+      winMessage.classList.add("--hidden");
+    }
+  }, [gameWon]);
 
   const getBoxSquares = (rowStart, colStart) => {
     let boxSquares = [];
@@ -28,6 +45,8 @@ export default function Square({ i, j, num, solution, board }) {
   };
 
   function handleClick(e) {
+    setClicked(false);
+
     // set all squares background to white
     const squares = document.querySelectorAll("td");
     squares.forEach((square) => (square.id = ""));
@@ -38,7 +57,10 @@ export default function Square({ i, j, num, solution, board }) {
 
     // find all squares in the same box
     const [rowNum, colNum] = [parseInt(row[4], 10), parseInt(col[4], 10)];
-    const boxSquares = getBoxSquares(rowNum-rowNum%3, colNum-colNum%3);
+    const boxSquares = getBoxSquares(
+      rowNum - (rowNum % 3),
+      colNum - (colNum % 3)
+    );
 
     // turn all adjacent squares' background color to light brown
     const adjacent = [...rowColSquares, ...boxSquares];
@@ -51,16 +73,18 @@ export default function Square({ i, j, num, solution, board }) {
   const handleKeyPress = (e) => {
     e.preventDefault();
     if (e.charCode < 49 || e.charCode > 57) return;
-    setGuess(e.charCode - 48)
-
-
+    setGuess(e.charCode - 48);
   };
 
   return (
     <td
       onClick={handleClick}
       onKeyPress={handleKeyPress}
-      className={num ? `row-${i} col-${j} preset` : `row-${i} col-${j} unset`}
+      className={
+        num
+          ? `row-${i} col-${j} preset square`
+          : `row-${i} col-${j} unset square`
+      }
       contentEditable={!gameWon && !num}
     >
       {guess}
