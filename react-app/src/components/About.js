c13c8294b8ec83906403e89d70f82ed58579ba9c
Connor <connorchenderson9@gmail.com>
2021-07-04 18:44:37 -0400

    about page and initial chess files

--- react-app/src/components/About.js
+++ react-app/src/components/About.js
@@ -3,19 +3,23 @@ import headshot from "../images/profile/headshot.jpg";
 import github from "../images/profile/github.png";
 import linkedIn from "../images/profile/linkedIn.jpeg";
 
-
 export default function About() {
   return (
     <>
       <div className="about-container">
         <img src={headshot} className="headshot"></img>
         <p>
-          This website is dedicated to a love of the logic for computers, board games, and writing board games for computers
+          This website is for enjoying of the logic of computers, board games,
+          and writing board games for computers
         </p>
       </div>
       <div className="about-links">
-          <a href="https://github.com/cch41"><img style={{heigth: "30px", width: "30px"}} src={github}/></a>
-          <a href="https://www.linkedin.com/in/connor-henderson-833504123/"><img style={{heigth: "30px", width: "30px"}} src={linkedIn}/></a>
+        <a href="https://github.com/cch41">
+          <img style={{ heigth: "30px", width: "30px" }} src={github} />
+        </a>
+        <a href="https://www.linkedin.com/in/connor-henderson-833504123/">
+          <img style={{ heigth: "30px", width: "30px" }} src={linkedIn} />
+        </a>
       </div>
     </>
   );
